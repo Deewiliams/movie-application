39c9504740c2ff407f905b38e1c1150b0c4b3104
Desire <desireirankundawilliams@gmail.com>
2022-10-11 01:57:04 +0200

    added a view movie detail button

--- src/Components/WatchList.js
+++ src/Components/WatchList.js
@@ -3,6 +3,7 @@ import { GlobalContext } from "./context/GlobalState";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
+import { Button } from "@material-ui/core";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import { truncate } from "../Utils/helpers";
@@ -42,6 +43,11 @@ const WatchList = () => {
                   image={`http://image.tmdb.org/t/p/w400${movie.poster_path}`}
                   title={movie.title}
                 />
+              </Link>
+              <Link to={`/movie/${movie.id}`}>
+                <Button variant="contained" color="primary" fullWidth>
+                  View Movie detail
+                </Button>
               </Link>
             </Card>
             <MovieControls movie={movie} />

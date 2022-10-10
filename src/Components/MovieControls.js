2198f997ab507361e4a3386c9c27cb44de021f63
Desire <desireirankundawilliams@gmail.com>
2022-10-11 01:49:42 +0200

    added icons

--- src/Components/MovieControls.js
+++ src/Components/MovieControls.js
@@ -1,15 +1,19 @@
 import React,{useContext} from "react";
 import { Button } from "@material-ui/core";
+import RemoveRedEyeIcon from '@material-ui/icons/RemoveRedEye';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import { GlobalContext } from "./context/GlobalState";
 
 const MovieControls = ({movie}) => {
     const { removeMovieFromWatchList,addMovieToWatched } = useContext(GlobalContext);
   return (
-    <div>
-      <Button onClick={() => addMovieToWatched(movie)} variant="contained">Default</Button>
-      <Button onClick={() => removeMovieFromWatchList(movie.id)} variant="contained" color="primary">
-        X
-      </Button>
+    <div style={{display: 'flex', justifyContent: 'center', cursor: 'pointer'}} >
+      <p onClick={() => addMovieToWatched(movie)} variant="contained">
+        <RemoveRedEyeIcon />
+      </p>
+      <p onClick={() => removeMovieFromWatchList(movie.id)} variant="contained" color="primary">
+        <DeleteForeverIcon />
+      </p>
     </div>
   );
 };

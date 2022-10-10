2057992d192357d498c50dd79209dece219f646b
Desire <desireirankundawilliams@gmail.com>
2022-10-10 19:38:00 +0200

    able to store info on the local storage

--- src/Components/context/GlobalState.js
+++ src/Components/context/GlobalState.js
@@ -2,8 +2,8 @@ import React,{useContext, useReducer, useEffect, createContext} from "react";
 import AppReducer from "./AppReducer";
 
 const initialState = {
-    watchList: [],
-    watched: []
+    watchList: localStorage.getItem('watchList') ? JSON.parse(localStorage.getItem('watchList')) : [],
+    watched: localStorage.getItem('watched') ? JSON.parse(localStorage.getItem('watchList')) : []
 }
 
 export const GlobalContext = createContext(initialState)
@@ -12,6 +12,11 @@ export const GlobalContext = createContext(initialState)
 export const GlobalProvider = props => {
     const [state, dispatch] = useReducer(AppReducer, initialState )
 
+    useEffect(() => {
+        localStorage.setItem('watchList', JSON.stringify(state.watchList))
+        localStorage.setItem('watched', JSON.stringify(state.watched))
+
+    },[state])
 
     //actions
 

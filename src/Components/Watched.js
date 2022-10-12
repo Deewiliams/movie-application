d808dfb43287b3dd776ef37f89204f972c8b2a35
Desire <desireirankundawilliams@gmail.com>
2022-10-12 09:39:04 +0200

    able to search for watched movies list

--- src/Components/Watched.js
+++ src/Components/Watched.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { GlobalContext } from "./context/GlobalState";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
@@ -8,8 +8,8 @@ import CardMedia from "@material-ui/core/CardMedia";
 import { truncate } from "../Utils/helpers";
 import { Link } from "react-router-dom";
 import WatchedMovieControls from "./WatchMovieControls";
-import TextMessage from './TextMessage'
-import { Container } from "@material-ui/core";
+import TextMessage from "./TextMessage";
+import { Container, TextField } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,36 +28,63 @@ const useStyles = makeStyles((theme) => ({
 
 const Watched = () => {
   const classes = useStyles();
-
+  const [query, setQuery] = useState("");
   const { watched } = useContext(GlobalContext);
 
-  if(watched.length < 1){
-    return <TextMessage message='watched'/>
+  if (watched.length < 1) {
+    return <TextMessage message="watched" />;
   }
 
   return (
     <div className={classes.root}>
+      <br />
       <Container>
-      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-helperText"
+            label="search watched movies"
+            variant="outlined"
+            type="text"
+            fullWidth
+            placeholder="Search for watched movies..."
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+        </Grid>
+      </Container>
+      <br />
+      <Container>
+        <Grid container spacing={3}>
           <>
-            {watched.map((movie) => (
-              <Grid item xs={12} sm={3} key={movie.id}>
-                <Card className={classes.root}>
-                  <CardHeader title={truncate(movie.title)} />
-                  <Link to={`/movie/${movie.id}`}>
-                    <CardMedia
-                      style={{ cursor: "pointer" }}
-                      className={classes.media}
-                      image={`http://image.tmdb.org/t/p/w400${movie.poster_path}`}
-                      title={movie.title}
-                    />
-                  </Link>
-                </Card>
-                <WatchedMovieControls movie={movie} />
-              </Grid>
-            ))}
+            {watched &&
+              watched
+                .filter((movie) => {
+                  if (query === "") {
+                    return movie;
+                  } else if (
+                    movie.title.toLowerCase().includes(query.toLowerCase())
+                  ) {
+                    return movie;
+                  }
+                })
+                .map((movie) => (
+                  <Grid item xs={12} sm={3} key={movie.id}>
+                    <Card className={classes.root}>
+                      <CardHeader title={truncate(movie.title)} />
+                      <Link to={`/movie/${movie.id}`}>
+                        <CardMedia
+                          style={{ cursor: "pointer" }}
+                          className={classes.media}
+                          image={`http://image.tmdb.org/t/p/w400${movie.poster_path}`}
+                          title={movie.title}
+                        />
+                      </Link>
+                    </Card>
+                    <WatchedMovieControls movie={movie} />
+                  </Grid>
+                ))}
           </>
-      </Grid>
+        </Grid>
       </Container>
     </div>
   );

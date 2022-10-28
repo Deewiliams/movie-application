ee2349c6bc1cb024011375242814e028758c7839
Desire <desireirankundawilliams@gmail.com>
2022-10-28 17:52:42 +0200

    removed unwanted code

--- src/Components/PopularMovies.js
+++ src/Components/PopularMovies.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid} from "@material-ui/core";
+import { Container, Grid } from "@material-ui/core";
 import DisplayPopularMovies from "./DisplayPopularMovies";
-
-
 
 export const PopularMovies = () => {
   const [fetchedMovies, setFetchedMovies] = useState([]);
@@ -24,47 +22,12 @@ export const PopularMovies = () => {
     <div>
       <Container>
         <Grid container spacing={8}>
-        {
-          fetchedMovies.map((movie) => (
-            <Grid item xs={12} sm={3}>
-            <DisplayPopularMovies movie={movie}  key={movie.id}/>
-            </Grid>
-          ))
-        }
-        </Grid>
-
-        {/* <Grid container spacing={8}>
           {fetchedMovies.map((movie) => (
-            <Grid item xs={12} sm={3} key={movie.id}>
-              <Card className={classes.root}>
-                <CardHeader
-                  title={truncate(movie.title)}
-                  subheader={movie.release_date.substring(0, 4)}
-                />
-                <Link to={`/movie/${movie.id}`}>
-                  <CardMedia
-                    className={classes.media}
-                    image={`http://image.tmdb.org/t/p/w400${movie.poster_path}`}
-                    title={movie.title}
-                  />
-                </Link>
-              </Card>
-              <br />
-              <Button
-                variant="contained"
-                color="primary"
-                disabled={disablewatchListMovie}
-                onClick={() => {
-                  addWatchListMovies(movie);
-                  setDisableSelectedWatchList(movie.id); 
-                }}
-                fullWidth
-              >
-                Add Watch list
-              </Button>
+            <Grid item xs={12} sm={3}>
+              <DisplayPopularMovies movie={movie} key={movie.id} />
             </Grid>
           ))}
-        </Grid> */}
+        </Grid>
       </Container>
     </div>
   );
